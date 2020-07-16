9f4a44021443eb6624e7753f4d3ebba19ceb5a70
shinnshi <shin.nshi3@gmail.com>
2020-07-16 18:26:23 +0900

    comp

--- src/FriendlyEats/FriendlyEats.Data.js
+++ src/FriendlyEats/FriendlyEats.Data.js
@@ -2,40 +2,79 @@ import * as firebase from "firebase/app"
 import "firebase/firestore";
 
 export const addRestaurant = (data) => {
-  /*
-    TODO: Implement adding a document
-  */
+  const collection = firebase.firestore().collection('restaurants');
+  return collection.add(data)
 }
 
 export const getAllRestaurants = () => {
-  /*
-    TODO: Retrieve list of restaurants
-  */
-}
+  const query = firebase.firestore()
+    .collection('restaurants')
+    .orderBy('avgRating', 'desc')// descending 
+    .limit(50)
 
+  return query;
+}
+// firebase > docChange
 export const getDocumentsInQuery = (query, renderer) => {
-  /*
-    TODO: Retrieve list of restaurants
-  */
+  return query.onSnapshot((snapshot) => {
+    if (!snapshot.size) return renderer.empty();
+
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'removed') {
+        renderer.remove(change.doc)
+      } else {
+        renderer.display(change.doc)
+      }
+    })
+  })
 }
 
 export const getRestaurant = (id) => {
-  /*
-    TODO: Retrieve a single restaurant
-  */
+  return firebase.firestore().collection('restaurants').doc(id).get();
 }
 
 export const getFilteredRestaurants = (filters) => {
-  /*
-    TODO: Retrieve filtered list of restaurants
-  */
+  let query = firebase.firestore().collection('restaurants');
+
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', 'filters.price.length')
+  }
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', 'filters.city.length')
+  }
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', 'filters.category.length')
+  }
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRating', 'desc');
+  }
+  return query
 }
 
+
 export const addRating = (restaurantID, rating) => {
-  /*
-    TODO: Retrieve add a rating to a restaurant
-  */
-}
+  const collection = firebase.firestore().collection('restaurants');
+  const document = collection.doc(restaurantID);
+  const newRatingDocument = document.collection('ratings').doc();
+
+  return firebase.firestore().runTransaction(function (transaction) {
+    return transaction.get(document).then(function (doc) {
+      const data = doc.data();
+
+      const newAverage =
+        (data.numRatings * data.avgRating + rating.rating) /
+        (data.numRatings + 1);
+
+      transaction.update(document, {
+        numRatings: data.numRatings + 1,
+        avgRating: newAverage
+      });
+      return transaction.set(newRatingDocument, rating);
+    });
+  });
+};
 
 export const getRating = (id) => {
   return firebase.firestore().collection('restaurants').doc(id).collection('ratings').orderBy('timestamp', 'desc');
--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
 // Replace this section with the config file of your own Firebase project. 
-const firebaseConfig = {
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD8j19zQhI3QLep14wm8QGUKvok69B_JAY",
+  authDomain: "friendlyeats-f18bb.firebaseapp.com",
+  databaseURL: "https://friendlyeats-f18bb.firebaseio.com",
+  projectId: "friendlyeats-f18bb",
+  storageBucket: "friendlyeats-f18bb.appspot.com",
+  messagingSenderId: "967412699983",
+  appId: "1:967412699983:web:ada951b9226958c636ab3a"
+};
 export default firebaseConfig;
